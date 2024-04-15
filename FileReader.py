06b4cd1d807861dd1589c3e73556356497322445
AmauryDubuc <amaurydubuctabouy@gmail.com>
2024-04-15 21:34:12 +1100

    Séparé 20,30 et 50

--- FileReader.py
+++ FileReader.py
@@ -1,6 +1,5 @@
 import os
 import random
-import shutil
 import math
 
 FILELIST = os.listdir("donnee")
@@ -37,38 +36,63 @@ def separerFichier(dico):
                     dico["listSm"].append(filename)
 
 
-def separer20(liste):
+def separer(liste):
 
-    liste_a_deplacer = []
+    liste_a_deplacer20 = []
+    liste_a_deplacer30 = []
+    liste_a_deplacer50 = []
 
     longueur = len(liste)
 
-    nbfichier = longueur * 20 / 100
+    nbfichier20 = math.floor(longueur * 20 / 100)
+    nbfichier30 = math.floor(longueur * 30 / 100)
 
     random.shuffle(liste)
 
-    for i in range(math.floor(nbfichier)):
-        liste_a_deplacer.append(liste[i])
+    for i in range(longueur):
+        if i < nbfichier20:
+            liste_a_deplacer20.append(liste[i])
+        elif i < nbfichier20 + nbfichier30:
+            liste_a_deplacer30.append(liste[i])
+        else:
+            liste_a_deplacer50.append(liste[i])
 
-    deplacerFichier(liste_a_deplacer)
+    deplacerFichier(liste_a_deplacer20, '20')
+    deplacerFichier(liste_a_deplacer30, '30')
+    deplacerFichier(liste_a_deplacer50, '50')
 
     
-
-
-def deplacerFichier(liste):
+def deplacerFichier(liste, pourcentage):
     source = os.getcwd() + '/donnee/'
 
-    destination = os.getcwd() + '/Donnée20/'
+    destination = os.getcwd() + '/Donnee' + pourcentage + '/'
 
     if not os.path.exists(destination):
         os.makedirs(destination)
 
     for i in range(len(liste)):
-        shutil.move(source + liste[i], destination)
-    
+        data_list = []
+        filepath = os.path.join(source, liste[i])
+        try:
+            with open(filepath, 'r') as fichier:
+                for ligne in fichier:
+                    valeurs = ligne.split()
+                    data_list.append(int(valeurs[1]))
+
+        except FileNotFoundError:
+            print("Le fichier spécifié n'a pas été trouvé.")
+            return None
+
+        except Exception as e:
+            print("Une erreur s'est produite :", e)
+            return None
+
+        with open(destination+liste[i], "w") as fichier_sortie:
+            for value in data_list:
+                fichier_sortie.write(f"{value}\n")
 
 
 separerFichier(dico)
 
 for i in dico.keys():
-    separer20(dico[i])
+    separer(dico[i])
